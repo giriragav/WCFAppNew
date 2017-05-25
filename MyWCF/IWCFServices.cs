8989cfc1f1d6ceeee0b0ffa4c528488f6b114c0c
venkat-giri <giri.ragav@gmail.com>
2017-05-25 17:49:42 -0500

    Initial Commit

---
+++ MyWCF/IWCFServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCF
+{
+    [ServiceContract]
+    interface IWCFServices
+    {
+        [OperationContract]
+         string GetTextData();
+      
+    }
+}
