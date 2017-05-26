5cbe4aeb95f707b0a3ac243cb47363b578d12038
venkat-giri <giri.ragav@gmail.com>
2017-05-26 17:15:58 -0500

    Refined WCF

---
+++ MyWCFClient/WCFForm.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyWCFClient.MyWCFService;
+
+namespace MyWCFClient
+{
+    public partial class WCFForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MyWCFService.WCFServicesClient WCFClient = new WCFServicesClient("BasicHttpBinding_IWCFServices");
+            Response.Write(WCFClient.GetTextData());
+
+        }
+    }
+}
